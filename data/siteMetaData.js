f8c480ad51578748204163da3fdd331ba2f03fc9
yubo.zhang <yubo.zhang@accenture.com>
2025-01-17 16:46:18 +0800

    factor: meta

---
+++ data/siteMetaData.js
@@ -0,0 +1,15 @@
+const siteMetaData = {
+  siteUrl: 'https://jasonzhang-dev.vercel.app/',
+  title: 'JasonZhang-Dev',
+  description: 'A blog created with Next.js, and Tailwind.css',
+  author: 'yubo.zhang',
+  locale: 'zh',
+  socialBanner: '/basic/',
+
+  email: 'zhangyubo56@163.com',
+  github: 'https://github.com/JasonZhang10',
+
+  headerTitle: 'YUBOOM',
+};
+
+module.exports = siteMetaData;
